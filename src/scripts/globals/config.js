2351d74f6db4f3fcbc4eb39897ba7c20489a3fa0
ikkromm18 <muhammadikkrom749@gmail.com>
2024-11-22 14:46:21 +0700

    inisialisasi template

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'e6aaa4bfc08131a6f80821c603fa568c',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
